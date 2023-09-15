1dc60cd047cf1a91a355e26f52ce07a1644878b5
Arthur Fernandez <arthur@snooget.com>
2023-09-15 11:46:04 -0700

    Created ui, and async function for making new todos

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,49 @@
 import React from "react";
+import Link from "next/link";
+import { prisma } from "../db";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+  "use server";
+
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title");
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/");
+}
 
 export default function page() {
-  return <div>Everything here will show up if we type in url/new</div>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form action={createTodo} className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        />
+        {/* The name="title" is important because it will be used inside of server actions - @9:56am 9/15 */}
+
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
