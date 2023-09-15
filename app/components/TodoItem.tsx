6b1f5cf36a30bba175f4dc13277d8e84eb6d2a75
Arthur Fernandez <arthur@snooget.com>
2023-09-15 09:53:47 -0700

    added new documentation regarding todoitem props

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,9 +1,11 @@
 type TodoItemProps = {
+  // Remember the typescript course, we have the type declared with todo item props that are assigned to a type. - @9:53am 9/15
   id: string;
   title: string;
   complete: Boolean;
 };
 export function TodoItem({ id, title, complete }: TodoItemProps) {
+  // todoitem props are called, {id}, {title}, completed has not yet been called - @9:53am 9/15
   return (
     <li className="flex gap-1 items-center">
       <input id={id} type="checkbox" className="cursor-pointer peer" />
